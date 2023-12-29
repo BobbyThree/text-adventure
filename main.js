051ff8dab5faf0c32100466bfbfd8ad9c98ffce0
bobbythree <mynameisbobbylewis@hotmail.com>
2023-12-28 20:18:11 -0600

    UI improvements, added some selectors to main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+let xp = 0;
+let hp = 50;
+let gp = 100;
+let inv = ['short sword', 'grappling hook'];
+
+const xpTxt = document.querySelector('#xp-txt');
+const hpTxt = document.querySelector('#hp-txt');
+const gpTxt = document.querySelector('#gp-txt');
+const invTxt = document.querySelector('#inv-txt');
+const txt = document.querySelector('#txt');
+
